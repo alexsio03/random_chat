2d446fd98a3f130481b1cbe8b0f139ccb2af92b2
Alex Warda <alexsio03@gmail.com>
2024-03-07 13:17:50 +0100

    Trying to add socket connections

--- App.js
+++ App.js
@@ -1,79 +1,122 @@
-import { StyleSheet, Text, SafeAreaView, TextInput } from "react-native";
+import { StyleSheet, Text, SafeAreaView, TextInput, Pressable } from "react-native";
+import React, {useState, useEffect} from 'react';
+import io from "socket.io-client";
 
 export default function App() {
-	return (
-		<SafeAreaView style={styles.container}>
-			<Texts
-				texts={text_msgs}
-			/>
-			<TextInput style={styles.input} placeholder="Send a message" />
-		</SafeAreaView>
-	);
+  const [text, setText] = useState('');
+  useEffect(() => {
+    const socket = io("http://localhost:4000");
+
+    socket.on('connect', () => {
+      console.log('Connected to server');
+    });
+
+    socket.on('disconnect', () => {
+      console.log('Disconnected from server');
+    });
+
+    return () => {
+      socket.disconnect();
+    };
+  }, []);
+
+  const handleSend = () => {
+    socket.emit("chat", text);
+    setText('');
+  }
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <Texts
+        texts={text_msgs}
+      />
+      <SafeAreaView style={styles.form}>
+        <TextInput value={text} style={styles.input} onChangeText={newText => setText(newText)} placeholder="Send a message" />
+        <Pressable style={styles.send} onPress={handleSend}><Text style={styles.send_text}>Send</Text></Pressable>
+      </SafeAreaView>
+    </SafeAreaView>
+  );
 }
 
 const Texts = ({ texts }) => (
-	<SafeAreaView style={styles.texts}>
-		{texts.map((value) =>
-			value.type === "me" ? (
-				<SafeAreaView style={styles.me}>
-					<Text style={styles.me_text}>{value.text}</Text>
-				</SafeAreaView>
-			) : (
-				<SafeAreaView style={styles.them}>
-					<Text style={styles.them_text}>{value.text}</Text>
-				</SafeAreaView>
-			),
-		)}
-	</SafeAreaView>
+  <SafeAreaView style={styles.texts}>
+    {texts.map((value) =>
+      value.type === "me" ? (
+        <SafeAreaView style={styles.me}>
+          <Text style={styles.me_text}>{value.text}</Text>
+        </SafeAreaView>
+      ) : (
+        <SafeAreaView style={styles.them}>
+          <Text style={styles.them_text}>{value.text}</Text>
+        </SafeAreaView>
+      ),
+    )}
+  </SafeAreaView>
 );
 
 const text_msgs = [
-  {type: "them", text: "Hello there!"},
-  {type: "me", text: "Hi!"},
-  {type: "me", text: "How are you?"},
-  {type: "them", text: "Very well, thank you."},
-  {type: "them", text: "And you?"},
+  { type: "them", text: "Hello there!" },
+  { type: "me", text: "Hi!" },
+  { type: "me", text: "How are you?" },
+  { type: "them", text: "Very well, thank you." },
+  { type: "them", text: "And you?" },
 ]
 
 const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		flexDirection: "column",
-		backgroundColor: "#ddd",
-		alignItems: "center",
-		justifyContent: "flex-end",
-		paddingBottom: 40,
-	},
-	texts: {
-		width: 380,
-	},
-	them: {
-		overflow: "hidden",
+  container: {
+    flex: 1,
+    flexDirection: "column",
+    backgroundColor: "#ddd",
+    alignItems: "center",
+    justifyContent: "flex-end",
+    paddingBottom: 40,
+  },
+  form: {
+    flexDirection: "row"
+  },
+  send: {
+    width: 40,
+    marginLeft: 10,
+    marginTop: 13,
+    height: 30,
+    borderRadius: 10,
+    backgroundColor: "#4287f5"
+  },
+  send_text: {
+    marginTop: 6,
+    marginLeft: 3,
+    color: "#fff"
+  },
+  texts: {
+    width: 380,
+  },
+  them: {
+    overflow: "hidden",
     flexDirection: "row",
-		justifyContent: "flex-start",
-	},
-	them_text: {
-		backgroundColor: "#aaa",
-		padding: 10,
-		color: "#fff",
-		margin: 5,
-	},
-	me: {
-		overflow: "hidden",
+    justifyContent: "flex-start",
+  },
+  them_text: {
+    backgroundColor: "#aaa",
+    padding: 10,
+    color: "#fff",
+    margin: 5,
+  },
+  me: {
+    overflow: "hidden",
     flexDirection: "row",
-		justifyContent: "flex-end",
-	},
-	me_text: {
-		color: "#fff",
-		padding: 10,
-		margin: 5,
-		backgroundColor: "#1446eb",
-	},
-	input: {
+    justifyContent: "flex-end",
+  },
+  me_text: {
+    color: "#fff",
+    padding: 10,
+    margin: 5,
+    backgroundColor: "#1446eb",
+  },
+  input: {
     marginTop: 10,
-		backgroundColor: "#eee",
-		width: 300,
-		padding: 10,
-		borderRadius: 10,
-	},
+    backgroundColor: "#eee",
+    width: 300,
+    padding: 10,
+    borderRadius: 10,
+  },
 });
